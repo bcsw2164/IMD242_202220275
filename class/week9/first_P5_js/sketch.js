f432df22fcec176f7e2c879168e25890f5e1d3f8
bcsw2164 <126849531+bcsw2164@users.noreply.github.com>
2024-11-04 18:56:51 +0900

    수업 끝

--- class/week9/first_P5_js/sketch.js
+++ class/week9/first_P5_js/sketch.js
@@ -1,8 +1,23 @@
+let tileNumX = 16;
+let tileNumY = 12;
+
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 100);
+  background('#333333');
+  //js에선 무조건 let으로 시작, int와 float를 구분하지않는다
+  noStroke();
+  fill('cornflowerblue');
+
+  for (let row = 0; row < tileNumY; row++) {
+    for (let column = 0; column < tileNumX; column++) {
+      let tileW = width / tileNumX;
+      let tileH = height / tileNumY;
+      let x = tileW * 0.5 + column * tileW;
+      let y = tileH * 0.5 + row * tileH;
+      ellipse(x, y, tileW, tileH);
+    }
+  }
 }
