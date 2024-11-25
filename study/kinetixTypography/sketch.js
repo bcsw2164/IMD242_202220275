d54439ae96f0c0ebdffe7d94b170bcbf62524ed3
bcsw2164 <126849531+bcsw2164@users.noreply.github.com>
2024-11-25 16:46:41 +0900

    키네틱타이포

--- study/kinetixTypography/sketch.js
+++ study/kinetixTypography/sketch.js
@@ -14,7 +14,7 @@ function setup() {
     sampleFactor: 0.1, //점의 밀도를 제어, 기본값 0.1
     simplifyThreshold: 0,
   });
-  angleMode(DEGREES);
+  angleMode(DEGREES); //각도를 '도' 단위로 변경 sin(angle)은 라디안단위가 기본
   //textToPoints(text,x,y,fontSize,options <<점을 생성하는 방식 조정할 수 있음)
 }
 
@@ -22,7 +22,8 @@ function draw() {
   background(220);
   for (let i = 0; i < points.length; i++) {
     //(points[i].x, points[i].y); //point는 캔버스에 점을 찍는 함수
-    ellipse(points[i].x + r * sin(angle + i * 30), points[i].y, 10, 10); //angle + i * 10 -> 각 점에 대해 각도가 다르게 적용되도록 하기 위해, 다른 속도나 위치에서 변동함
+    ellipse(points[i].x + r * sin(angle + i * 15), points[i].y, 10, 10); //angle + i * 10 -> 각 점에 대해 각도가 다르게 적용되도록 하기 위해, 다른 속도나 위치에서 변동함
+    // 원형 경로를 따라 움직이는 점을 계산할 때 sin()함수 사용
     //i * n < 수치 변경해보세여
   }
   angle += 10; //angle값을 10씩 증가시킨다
